b7b5fb1366bc534a035854484e618becf3fe84e8
Zachary Peck <peckzl@clarkson.edu>
2015-10-22 23:46:27 -0400

    flask-intro

--- app.py
+++ app.py
@@ -1,19 +1,50 @@
-# ---- Flask Hello World ---- #
+# --- Flask Hello World ---#
 
-#import Flask class from the flask package
+# import the Flask class from the flask module
 from flask import Flask
 
-#create the app object
+# create the application object
 app = Flask(__name__)
 
-#user decorators to link the function to a url
+#error handling
+app.config["DEBUG"] = True
+
+# use decorators to link the function to a url
 @app.route("/")
 @app.route("/hello")
 
-#define the view using a function, which returns a string
+# define the view using a function, which returns a string
 def hello_world():
-	return "Hello, World!"
+    return "Hello, World!?!?!?!"
 
-#start the development server using the run() method
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+	print value + 1
+	return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+	print value + 1
+	return "correct"
+
+#dynamic route that accepts slashes
+@app.route("/path/<path:value>")
+def path_type(value):
+	print value
+	return "correct"
+
+@app.route("/name/<name>")
+def index(name):
+	if name.lower() == "michael":
+		return "Hello, {}".format(name)
+	else:
+		return "Not Found", 404
+
+# start the development server using the run() method
 if __name__ == "__main__":
-	app.run()
+    app.run()
